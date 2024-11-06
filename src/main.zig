551018dc2e315f3a543e9c7733d411ad96d404e2
DiegoDev <144474823+DiegoDev2@users.noreply.github.com>
2024-11-05 20:56:06 -0400

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 //! By convention, main.zig is where your main function lives in the case that
 //! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+//! is to delete this file and start with root.zig instead.,
 const std = @import("std");
 
 pub fn main() !void {
